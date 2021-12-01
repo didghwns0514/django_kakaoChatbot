16e7eb30d544fb9ce0f24f6a409d8016d80b6c36
HoJun Yang <didghwns0514@gmail.com>
2021-12-01 09:43:47 +0900

    init,config(Initialize project/Django secrets added): Add;

--- parseapp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParseappConfig(AppConfig):
-    name = 'parseapp'
-
-    def ready(self):
-        from BusinessLogic import BL_Scheduler
-        BL_Scheduler.start_news()
