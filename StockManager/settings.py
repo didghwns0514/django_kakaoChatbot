206a15bbe3d1cf0a661ea4cf323413715867d2f8
HoJun Yang <didghwns0514@gmail.com>
2021-12-01 10:26:53 +0900

    config(Django settings.py): Added secret settings

--- StockManager/settings.py
+++ StockManager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+import StockManager.subSettings as CONFI
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv( 'DJANGO_SECRET')
+SECRET_KEY = CONFI.DJANGO_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'StockManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', #1
+        'NAME': 'stockmanager', #2
+        'USER': CONFI.DB_USERNAME, #3
+        'PASSWORD': CONFI.DB_PASSWORD,  #4
+        'HOST': 'hjyang.iptime.org',   #5
+        'PORT': '44441', #6
     }
 }
 
---
+++ StockManager/subSettings.py
@@ -0,0 +1,17 @@
+import os
+
+DJANGO_SECRET = None
+DB_USERNAME = None
+DB_PASSWORD = None
+
+try:DJANGO_SECRET = os.environ(['DJANGO_SECRET'])
+except:DJANGO_SECRET = os.environ.get('DJANGO_SECRET')
+
+
+try:DB_USERNAME = os.environ(['DB_USERNAME'])
+except:DB_USERNAME = os.environ.get('DB_USERNAME')
+
+try:DB_PASSWORD = os.environ(['DB_PASSWORD'])
+except:DB_PASSWORD = os.environ.get('DB_PASSWORD')
+
+print(f'DJANGO_SECRET : {DJANGO_SECRET}')
