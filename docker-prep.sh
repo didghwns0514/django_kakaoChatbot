3d500ad2b006ea5384c2b63d8b95baf297da179c
HoJun Yang <didghwns0514@gmail.com>
2021-12-01 10:41:05 +0900

    config(docker-prep.sh): Update; Dockerfile library

--- docker-prep.sh
+++ docker-prep.sh
@@ -1,11 +1,11 @@
 pip install --upgrade pip
 pip install -r requirements.txt
-pip install django-cors-headers
-pip install mysqlclient
-pip install gunicorn
-pip install apscheduler
-pip install selenium
-pip install bs4 lxml
+#pip install django-cors-headers
+#pip install mysqlclient
+#pip install gunicorn
+#pip install apscheduler
+#pip install selenium
+#pip install bs4 lxml
 
 python3 manage.py collectstatic
 python3 manage.py makemigrations
