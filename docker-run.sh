e197118a72fd7d47dcf0f39b9e214e095141ad19
HoJun Yang <didghwns0514@gmail.com>
2021-12-03 13:22:02 +0900

    build,config(docker-run.sh):Update;Port fixed

--- docker-run.sh
+++ docker-run.sh
@@ -17,7 +17,7 @@ echo "Current PWD : $PWD"
 echo "Current files ls : "
 ls
 
-docker run  --name StockManager-Django-Server -d -p 5552:8080  \
+docker run  --name StockManager-Django-Server -d -p 5552:5552  \
   -e DJANGO_SECRET='django-insecure-dgbp!q_yauu-j)udgh*wa(ml-epuw#_&8hh=afgs_ocy_)#vtl'  \
   -e DB_USERNAME='root' \
   -e DB_PASSWORD='d@12YinYang' \
