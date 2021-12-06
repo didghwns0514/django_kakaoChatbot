ff839a8e23864c883378ca8dca857c147a9842bb
HoJun Yang <didghwns0514@gmail.com>
2021-12-06 11:19:31 +0900

    issue BASE DIR FIX - 8(collect static --noinput

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,7 +1,7 @@
 pip list
 python3 manage.py makemigrations
 python3 manage.py migrate
-#python3 manage.py collectstatic --noinput
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput
+#python3 manage.py collectstatic
 
 #python3 gunicorn StockManager.wsgi --bind=0:5552
