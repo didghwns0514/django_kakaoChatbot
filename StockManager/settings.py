7dac1c3b8a68857f51e048c86661eda71dc2a52c
HoJun Yang <didghwns0514@gmail.com>
2021-12-06 11:10:25 +0900

    issue BASE DIR FIX - 7(static dirs setting fixed)

--- StockManager/settings.py
+++ StockManager/settings.py
@@ -142,18 +142,18 @@ USE_TZ =  False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-#STATIC_DIR = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     STATIC_DIR,
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 print(f'BASE_DIR :{BASE_DIR}')
 print(f'ROOT_DIR :{ROOT_DIR}')
 print(f'STATIC_URL :{STATIC_URL}')
 # print(f'STATIC_DIR :{STATIC_DIR}')
 # print(f'STATICFILES_DIRS :{STATICFILES_DIRS}')
-print(f'STATIC_ROOT :{STATIC_ROOT}')
+# print(f'STATIC_ROOT :{STATIC_ROOT}')
 
 
 # Default primary key field type
