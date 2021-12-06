34939c1625385edfd98130fd6f5daf5daca4ea2e
HoJun Yang <didghwns0514@gmail.com>
2021-12-06 16:53:13 +0900

    build,refactor(scheduler.py):Update;Noraml timezone

--- appStockInfo/scheduler.py
+++ appStockInfo/scheduler.py
@@ -3,14 +3,14 @@ from appStockInfo.jobs import JobDatetime1, JobDatetime2
 
 
 def taskStockKR():
-    scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler(timezone="Asia/Seoul")
     scheduler.add_job(JobDatetime1, 'interval', seconds=5 )
 
     scheduler.start()
 
 
 def taskStockUS():
-    scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler(timezone="Asia/Seoul")
     scheduler.add_job(JobDatetime2, 'interval', seconds=10 )
 
     scheduler.start()
