accb6aa802b0b2a8348907d36d162fb6fdee10b4
HoJun Yang <didghwns0514@gmail.com>
2022-01-08 08:18:08 +0900

    build(appStockAccount):Update; Templates

--- appStockAccount/urls.py
+++ appStockAccount/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
 	path('', views.index, name="index"),
-	path('signup/', views.signup),
+	path('signup/', views.signup, name='가입'),
 	path('check1/', views.index),
 	path('check2/', views.index_check),
 ]
